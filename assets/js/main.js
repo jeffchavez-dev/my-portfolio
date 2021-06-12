4063b1456d54e33e09b9bb5026a2111a88bb88d1
Jeff Chavez <jeff.chavez0828@gmail.com>
2021-06-12 10:32:51 +0800

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,7 +51,7 @@ sr.reveal('.home__social-icon',{ interval: 200});
 sr.reveal('.skills__subtitle',{}); 
 sr.reveal('.skills__text',{}); 
 sr.reveal('.skills__data',{interval: 200}); 
-sr.reveal('.skills__img',{delay: 600});
+// sr.reveal('.skills__img',{delay: 600});
 
 /*SCROLL WORK*/
 sr.reveal('.work__img',{interval: 200}); 

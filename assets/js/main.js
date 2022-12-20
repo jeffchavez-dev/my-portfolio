f24698abc8c9d08c92dcce166f1c9b6af9cb3adb
Jeff Chavez <jeff.chavez0828@gmail.com>
2022-12-20 14:42:17 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -65,28 +65,14 @@ var control2 = document.getElementById('control2');
 var control3 = document.getElementById('control3');
 
 
-control1.onclick=function(){
-    testimonials.style.transform = "translateX(870px)";
-    control1.classList.add("active");
-    control2.classList.remove("active");
-    control3.classList.remove("active");
-}
-
-control2.onclick=function(){
-    testimonials.style.transform = "translateX(0px)";
-    control1.classList.remove("active");
-    control2.classList.add("active");
-    control3.classList.remove("active");
-}
-
-control3.onclick=function(){
-    testimonials.style.transform = "translateX(-870px)";
-    control1.classList.remove("active");
-    control2.classList.remove("active");
-    control3.classList.add("active");
-}
 
 
+
+const changeBgBtn = document.querySelector('.change-bg-btn')
+changeBgBtn.addEventListener('click', () =>{
+    document.body.style.backgrounColor = 'white';
+    alert('color changed')
+})
 
 
 
